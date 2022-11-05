49b656a4670c7433138ee83d96bdea8cb055aadf
faulty <didier@faulty.nl>
2022-11-05 21:49:35 +0100

    fix: follow PEP standard :)

--- src/lib/helpers.py
+++ src/lib/helpers.py
@@ -1,12 +1,12 @@
 import os
 
 
-def envBYN(key: str, val=None):
+def envBYN(key: str, val: object = None) -> True or False or None:
+    # envBYN (Binary Yes No) is a helper function to convert a string to a boolean
     if val is None:
         val = os.getenv(key)
     elif type(val) is not str:
         val = str(val)
-    # envBYN (Binary Yes No) is a helper function to convert a string to a boolean
     yesvals = ['true', 'yes', 'y', '1']
     novals = ['false', 'no', 'n', '0']
     if val.lower() in yesvals:
