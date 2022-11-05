a397bf95133f476cdd64e15aeb2a09bbdd9bf330
faulty <didier@faulty.nl>
2022-11-05 21:36:23 +0100

    feat: installer now unzips files I also remerged it to main to stop duplicate code.

--- src/main.py
+++ src/main.py
@@ -1,24 +1,43 @@
+import os
+import sys
+from zipfile import ZipFile
+
 from lib.Downloader import Downloader
 from lib.KnowWhere import KnowWhere
 
 kw = KnowWhere()
 base_daemon_url = "https://github.com/cybfi/daemon/releases/latest/download/"
 
-def UNIX():
-    ddl = Downloader(base_daemon_url + "unix.zip", kw.programdata + "/daemon.zip")
+
+def main():
+    if kw.platform == "nt":
+        ddl = Downloader(base_daemon_url + "windows.zip", kw.programdata + "/daemon.zip")
+    elif kw.platform == "posix":
+        ddl = Downloader(base_daemon_url + "unix.zip", kw.programdata + "/daemon.zip")
+    else:
+        raise Exception("Unknown platform")
     ddl.safe_download()
 
-def WINDOWS():
-    ddl = Downloader(base_daemon_url + "windows.zip", kw.programdata + "/daemon.zip")
-    ddl.safe_download()
+    # unzip
+    with ZipFile(kw.programdata + "/daemon.zip", "r") as zipObj:
+        zipObj.extractall(kw.programdata + "/daemon")
 
 
+
+def permission_check():
+    if kw.platform == "nt":
+        # check if running as admin and if not, restart as admin
+        import ctypes
+        if not ctypes.windll.shell32.IsUserAnAdmin():
+            import subprocess
+            subprocess.run(["powershell", "-Command", "Start-Process", "-Verb", "RunAs", "-FilePath", sys.executable, "-ArgumentList", " ".join(sys.argv)])
+            exit(0)
+    elif kw.platform == "posix":
+        # check if running as root and if not, restart as root
+        if os.geteuid() != 0:
+            print("Please run as root")
+            exit(0)
 
 if __name__ == '__main__':
-    if kw.platform == "posix":
-        UNIX()
-    elif kw.platform == "nt":
-        WINDOWS()
-    else:
-        print("Unknown / Unsupported platform.")
-        exit(1)
+    permission_check()
+    main()
